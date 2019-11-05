5cb5b5c89bce6ce6e7039507590a0b5932243a7c
Rodrigo <ratorres7@gmail.com>
2019-11-05 21:52:01 -0200

    Adding loop over array and map

--- person.js
+++ person.js
@@ -1,23 +1,15 @@
 const person = {
     name: 'Rodrigo',
     age: 28,
-
-    // The following function won't work, because this will refer to the global scope, and not to the object scope
-    greet_wrong:() =>{
-        console.log('Hi, I am ' + this.name);
-    },
-
-    // The following function will work
-    greet_opt1: function () {
-        console.log('Hi, I am ' + this.name);
-    },
-
-    // The following function will also work
-    greet_opt2() {
-        console.log('Hi, I am ' + this.name);
+    greet() {
+        console.log('Hi, I am ' + this.name)
     }
 };
 
-person.greet_wrong();
-person.greet_opt1();
-person.greet_opt2();
+const hobbies = ['Sports', 'Cooking'];
+
+for (let hobby of hobbies){
+    console.log(hobby)
+}
+
+console.log(hobbies.map(hobby=>hobby.toUpperCase()));
