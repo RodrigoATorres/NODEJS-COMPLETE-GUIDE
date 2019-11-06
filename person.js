c4d265edef4a14961227d051091b09d0f8447e31
Rodrigo <ratorres7@gmail.com>
2019-11-05 22:08:08 -0200

    Examples on how to copy and how not to copy arrays

--- person.js
+++ person.js
@@ -17,4 +17,22 @@ console.log(hobbies.map(hobby=>hobby.toUpperCase()));
 console.log(hobbies);
 
 // Constant arrays may be changed, as the variable will still be pointing to the same array. We are just changing the array, and not where the constant is pointing to
+
+// How to copy arrays
+const copiedArray1 = hobbies.slice();
+hobbies.push('Flying')
+console.log('Copy 1');
+console.log(copiedArray1);
+console.log(hobbies);
+
+const copiedArray2 = [...hobbies];
+hobbies.push('Playing Soccer')
+console.log('Copy 2');
+console.log(copiedArray2);
+console.log(hobbies);
+
+const copiedArray3 = hobbies;
 hobbies.push('Programming')
+console.log('Copy 3');
+console.log(copiedArray3);
+console.log(hobbies);
